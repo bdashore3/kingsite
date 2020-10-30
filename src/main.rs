147dd65835483a3d97482d12c7a369e85012c780
kingbri <bdashore3@gmail.com>
2020-10-29 23:23:11 -0400

    Initial commit
    Signed-off-by: kingbri <bdashore3@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+#[macro_use] extern crate rocket;
+
+use rocket::{Request, response::{content::Html, NamedFile, Responder}};
+
+#[get("/")]
+async fn index<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("html/index.html").await.ok()
+}
+
+#[get("/css/index.css")]
+async fn index_css<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("css/index.css").await.ok()
+}
+
+#[get("/images/favicon.png")]
+async fn favicon<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/favicon.png").await.ok()
+}
+
+#[catch(404)]
+async fn not_found(req: &Request<'_>) -> String {
+    format!("Whoops. That's a 404: {}", req)
+}
+
+#[launch]
+async fn rocket() -> rocket::Rocket {
+    rocket::ignite()
+        .mount("/", routes![index, index_css, favicon])
+        .register(catchers![not_found])
+}
