a06405a47288a42519a10d0fa07b4c130398c391
kingbri <bdashore3@gmail.com>
2020-11-02 23:29:30 -0500

    Add drop shadow to nav bar
    Adding a drop shadow to the navigation bar gives a nice effect when
    reading text.
    
    Also, the div containers per section of the website were extending
    off the screen, so fix that using flex and a method to fill up the remaining
    screen size
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -14,9 +14,49 @@ async fn tailwind<'a>() -> impl Responder<'a, 'static> {
     NamedFile::open("css/tailwind.css").await.ok()
 }
 
+#[get("/css/test.css")]
+async fn test_css<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("css/test.css").await.ok()
+}
+
 #[get("/images/favicon.png")]
 async fn favicon<'a>() -> impl Responder<'a, 'static> {
     NamedFile::open("images/favicon.png").await.ok()
+}
+
+#[get("/images/Email.png")]
+async fn email_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Email.png").await.ok()
+}
+
+#[get("/images/GitHub.png")]
+async fn github_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/GitHub.png").await.ok()
+}
+
+#[get("/images/Twitch.jpg")]
+async fn twitch_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Twitch.jpg").await.ok()
+}
+
+#[get("/images/Youtube.png")]
+async fn youtube_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Youtube.png").await.ok()
+}
+
+#[get("/images/Twitter.png")]
+async fn twitter_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Twitter.png").await.ok()
+}
+
+#[get("/images/Discord.png")]
+async fn discord_image<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Discord.png").await.ok()
+}
+
+#[get("/Contact-bg.webm")]
+async fn contact_webm<'a>() -> impl Responder<'a, 'static> {
+    NamedFile::open("images/Contact-bg.webm").await.ok()
 }
 
 #[catch(404)]
@@ -27,6 +67,18 @@ async fn not_found(req: &Request<'_>) -> String {
 #[launch]
 async fn rocket() -> rocket::Rocket {
     rocket::ignite()
-        .mount("/", routes![index, tailwind, favicon])
+        .mount("/", routes![
+            index,
+            tailwind,
+            test_css,
+            favicon,
+            email_image,
+            github_image,
+            twitch_image,
+            youtube_image,
+            twitter_image,
+            discord_image,
+            contact_webm,
+        ])
         .register(catchers![not_found])
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
         whitesmoke: '#f5f5f5',
         darkbg: '#0f151c',
         lightbg: '#243242',
+        twitch: '#593e85',
+        email: '#3b64af',
+        twitter: '#50abf1',
+        github: '#2b2727',
+        discord: '#36393f',
       },
       fontFamily: {
         header: ['Montserrat', 'Calibri'],
@@ -21,7 +26,10 @@ module.exports = {
       },
       height: {
         '18': "4.5rem",
-      }
+      },
+      boxShadow: {
+        drop: '0 0px 10px 8px #0f151c',
+      },
     },
   },
   variants: {},
