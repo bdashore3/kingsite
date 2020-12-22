2659e109fe3b807729ec18fc0081debf7e58793f
kingbri <bdashore3@gmail.com>
2020-12-22 16:43:47 -0500

    Work: Modify to use Masonry
    Masonry layout is a responsive layout format and since the work
    page of the website is structured via cards, make a single card
    and organize them through masonry.
    
    This is flexible compared to a CSS grid which was used previously.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- components/work.jsx
+++ components/work.jsx
@@ -1,127 +1,78 @@
-// TODO: Use masonry and rework how each Work card is displayed/styled
-export default function Work() {
-    return(
-        <div className="flex flex-flow-c min-h-screen">
-        <div id="work" className="h-18"></div>
-        <div className="inline-flex flex-flow-c flex-grow overflow-visible">
-          <h1
-            className="text-yellow-300 text-center text-6xl font-header font-semibold tracking-wide p-3"
+import Masonry from 'react-masonry-component';
+
+const masonryOptions = {
+  gutter: 65,
+  horizontalOrder: true,
+  fitWidth: true
+};
+
+function WorkCard(props) {
+  let buttonText = "View source"
+
+  if (props.url.includes("https://github.com")) {
+    buttonText = "View source code"
+  }
+
+  return(
+    <div className="grid-item p-5">
+      <h1 className="absolute underline text-3xl">{props.name}</h1>
+      <p className="relative inline-flex flex-grow align-center text-xl font-paragraph">{props.description}</p>
+      <div className="inline-flex absolute bottom-4">
+        <a href={props.url} target="_blank">
+          <button
+            className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
           >
-            My Work
-          </h1>
-          <div className="inline-flex flex-grow items-center justify-items-center">
-            <div className="grid-container gap-y-16 gap-x-16 w-full px-32">
-              <div className="grid-item p-5">
-                <h1 className="underline text-3xl">RoyalGuard</h1>
-                <p className="mt-16 text-xl font-paragraph">
-                  Make your discord administration experience simple and easy.
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a
-                    href="https://github.com/bdashore3/RoyalGuard"
-                    target="_blank"
-                  >
-                    <button
-                      className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="grid-item p-5">
-                <h1 className="underline text-3xl">CourtJester</h1>
-                <p className="mt-16 text-xl font-paragraph">
-                  A multipurpose bot which enhances your discord server
-                  experience.
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a
-                    href="https://github.com/bdashore3/CourtJester"
-                    target="_blank"
-                  >
-                    <button
-                      className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="grid-item p-5">
-                <h1 className="underline text-3xl">Simple Retweets</h1>
-                <p className="mt-16 text-xl font-paragraph">
-                  A bot that retweets mentioned posts on a configured account.
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a
-                    href="https://github.com/bdashore3/Simple-Retweets"
-                    target="_blank"
-                  >
-                    <button
-                      className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="grid-item p-5">
-                <h1 className="text-3xl underline">KingBot-Twitch</h1>
-                <p className="mt-16 text-xl font-paragraph">
-                  My personal bot for Twitch. Is currently being rewritten in
-                  Rust.
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a href="https://github.com/bdashore3/KingBot" target="_blank">
-                    <button
-                      className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="grid-item p-5">
-                <h1 className="text-3xl underline">KingKernel</h1>
-                <p className="mt-16 text-xl font-paragraph">
-                  A custom kernel for the Google Pixel lineup focused on making
-                  life simpler.
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a href="https://github.com/King-Kernel/" target="_blank">
-                    <button
-                      className="text-lg rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-              <div className="grid-item p-5">
-                <h1 className="text-3xl underline">TypeMap Revitalized</h1>
-                <p className="text-xl mt-10 font-paragraph">
-                  A hashmap, but uses types as keys. You can put anything in
-                  there!
-                  <br />
-                  (Collaboration with serenity-rs)
-                </p>
-                <div className="inline-flex absolute bottom-4">
-                  <a
-                    href="https://github.com/bdashore3/typemap_rev"
-                    target="_blank"
-                  >
-                    <button
-                      className="text-xl rounded-md px-3 py-2 duration-500 text-whitesmoke border hover:text-black hover:bg-whitesmoke"
-                    >
-                      View source code
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            {buttonText}
+          </button>
+        </a>
       </div>
-    )
+    </div>
+  )
+}
+
+export default function Work() {
+  return(
+    <div className="flex flex-flow-c min-h-screen">
+    <div id="work" className="h-18"></div>
+    <div className="inline-flex flex-flow-c flex-grow overflow-visible">
+      <h1
+        className="text-yellow-300 text-center text-6xl font-header font-semibold tracking-wide p-3"
+      >
+        My Work
+      </h1>
+      <Masonry options={masonryOptions} className="max-w-full my-0 mx-auto p-12">
+        <WorkCard
+          name="RoyalGuard"
+          description="Makes your discord administration experience simple and easy."
+          url="https://github.com/bdashore3/RoyalGuard"
+        />
+        <WorkCard
+          name="CourtJester"
+          description="A multipurpose bot which enhances your discord server experience."
+          url="https://github.com/bdashore3/CourtJester"
+        />
+        <WorkCard
+          name="Simple Retweets"
+          description="A bot that retweets mentioned posts on a configured account."
+          url="https://github.com/bdashore3/RoyalGuard"
+        />
+        <WorkCard
+          name="KingBot-Twitch"
+          description="My personal bot for Twitch. Is currently being rewritten in Rust."
+          url="https://github.com/bdashore3/KingBot"
+        />
+        <WorkCard
+          name="KingKernel"
+          description="A custom kernel for the Google Pixel lineup with simplicity as the main feature."
+          url="https://github.com/King-Kernel"
+        />
+        <WorkCard
+          name="RegalPack"
+          description="A minecraft modpack that allows you to choose your own story."
+          url="https://www.curseforge.com/minecraft/modpacks/regalpack"
+        />
+      </Masonry>
+    </div>
+    </div>
+  )
 }
