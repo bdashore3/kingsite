bd550e2e11f05d588a5ac4f38268fa6a14ecb694
kingbri <bdashore3@gmail.com>
2020-12-26 20:13:37 -0500

    Add OpenGraph meta tags
    These allow for rich embeds on discord and twitter.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,12 @@ export default function IndexPage() {
     <div>
       <Head>
         <link rel="icon" type="image/svg" href="/favicon.svg" />
+        <meta property="og:title" content="Kingbri's website" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://kingbri.me" />
+        <meta property="og:description" content="Hey there, I'm Bri! Take a look at my site!" />
+        <meta property="og:image" content="https://avatars1.githubusercontent.com/u/8082010?s=460&u=6f29bfc7183955182abe0625dc1cc8054db0f2b9&v=4" />
+        <meta name="theme-color" content="ffd700" />
         <title>Kingbri's website</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Open+Sans&family=Raleway&display=swap"
