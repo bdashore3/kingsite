5827929a068ce0b890498dc4fdd2230bb095696b
kingbri <bdashore3@gmail.com>
2020-12-26 20:33:23 -0500

    Fix meta description
    Lighthouse flagged this for SEO.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,8 @@ export default function IndexPage() {
         <meta property="og:title" content="Kingbri's website" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://kingbri.me" />
-        <meta property="og:description" content="Hey there, I'm Bri! Take a look at my site!" />
+        <meta property="og:description" content="My name's Brian. This is my website. I make a lot of cool things. Come take a look!" />
+        <meta name="Description" content="My name's Brian. This is my website. I make a lot of cool things. Come take a look!" />
         <meta property="og:image" content="https://avatars1.githubusercontent.com/u/8082010?s=460&u=6f29bfc7183955182abe0625dc1cc8054db0f2b9&v=4" />
         <meta name="theme-color" content="ffd700" />
         <title>Kingbri's website</title>
