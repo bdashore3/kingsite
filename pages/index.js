00d66543ef3a81917c6b4ae8153815097d26e9d8
kingbri <bdashore3@gmail.com>
2020-12-26 20:38:53 -0500

    Meta: Fix hex color used
    Discord will not render the color for the embed unless it looks
    like this: `#hex`
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function IndexPage() {
         <meta property="og:description" content="My name's Brian. This is my website. I make a lot of cool things. Come take a look!" />
         <meta name="Description" content="My name's Brian. This is my website. I make a lot of cool things. Come take a look!" />
         <meta property="og:image" content="https://avatars1.githubusercontent.com/u/8082010?s=460&u=6f29bfc7183955182abe0625dc1cc8054db0f2b9&v=4" />
-        <meta name="theme-color" content="ffd700" />
+        <meta name="theme-color" content="#ffd700" />
         <title>Kingbri's website</title>
         <link
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Open+Sans&family=Raleway&display=swap"
