37f99467fb0056a241d02e8e93d2bb911bcca666
kingbri <bdashore3@gmail.com>
2021-02-20 12:50:12 -0500

    Blog: Fix post updating mechanism
    When fallback isn't enabled on the blog, the posts do not regenerate
    through SSG since there is no stimulus to reload the post content.
    
    This results on a post entry showing up on the blog homepage, but
    a 404 is fired when the user clicks on the post since the content wasn't
    populated at build time.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- pages/blog/post/[slug].tsx
+++ pages/blog/post/[slug].tsx
@@ -2,22 +2,38 @@ import { sanity } from '@/lib/sanity';
 import { FullPost, Post } from '@/models/schema';
 import BlogPostContent from '@/components/blog/post';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import FourOhFour from 'pages/404';
 
 interface Props {
   post: FullPost;
 }
 
 export default function BlogPost({ post }: Props) {
+  const router = useRouter();
+
+  if (!router.isFallback && !post.slug) {
+    return <FourOhFour />;
+  }
+
   return (
     <>
-      <Head>
-        <title>{post.title}</title>
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Open+Sans&family=Raleway&display=swap"
-          rel="stylesheet"
-        />
-      </Head>
-      <BlogPostContent postData={post} />
+      {router.isFallback ? (
+        <h1 className="flex flex-col justify-center text-center font-navigation min-h-screen text-4xl md:text-5xl lg:text-7xl p-8">
+          Loading...
+        </h1>
+      ) : (
+        <>
+          <Head>
+            <title>{post.title}</title>
+            <link
+              href="https://fonts.googleapis.com/css2?family=Montserrat:wght@600&family=Open+Sans&family=Raleway&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
+          <BlogPostContent postData={post} />
+        </>
+      )}
     </>
   );
 }
@@ -33,21 +49,16 @@ export async function getStaticProps({ params }: Params) {
   const post = await sanity.fetch<FullPost>(
     `*[_type == "post" && slug.current == $slug][0]{
       title,
+      slug,
       'author': author->{name, 'image': image.asset->url, 'bio': bio[]},
       body
     }`,
     { slug }
   );
 
-  if (!post) {
-    return {
-      notFound: true
-    };
-  }
-
   return {
     props: {
-      post
+      post: post || null
     },
     revalidate: 1
   };
@@ -62,6 +73,6 @@ export async function getStaticPaths() {
           slug: post.slug
         }
       })) || [],
-    fallback: false
+    fallback: true
   };
 }
