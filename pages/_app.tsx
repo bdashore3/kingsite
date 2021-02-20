746cc876f6ec06fb247c164de69d529bbfdf1a01
kingbri <bdashore3@gmail.com>
2021-02-19 22:04:56 -0500

    Reference preflight overrides in app
    Nextjs needs to know that there's another CSS file.
    
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { AppProps } from 'next/app';
 
 import '@/styles/index.css';
+import '@/styles/preflight-defaults.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
