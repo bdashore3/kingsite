d030eda149717b5c423f86752e06ab6b829f8231
kingbri <bdashore3@gmail.com>
2021-11-28 16:55:55 -0500

    kingsite: Upgrade dependencies
    Signed-off-by: kingbri <bdashore3@gmail.com>

--- lib/sanity.ts
+++ lib/sanity.ts
@@ -2,6 +2,7 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { createClient, createImageUrlBuilder } from 'next-sanity';
 
 const config = {
+  apiVersion: '2021-08-31',
   dataset: 'production',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || '',
   useCdn: true
